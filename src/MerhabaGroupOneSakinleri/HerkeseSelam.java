c752d958c8d31f01768210ed24375f239e3a4b4e
Cenk K <89126231+CenkK67@users.noreply.github.com>
2023-01-21 16:42:09 +0300

    Update HerkeseSelam.java

--- src/MerhabaGroupOneSakinleri/HerkeseSelam.java
+++ src/MerhabaGroupOneSakinleri/HerkeseSelam.java
@@ -3,5 +3,6 @@ package MerhabaGroupOneSakinleri;
 public class HerkeseSelam {
     public static void main(String[] args) {
         System.out.println("Grup 1 Üyeleri Nasılsınız? İşler Nasıl Gidiyor?");
+        //Sağol Rıdvan.
     }
 }
