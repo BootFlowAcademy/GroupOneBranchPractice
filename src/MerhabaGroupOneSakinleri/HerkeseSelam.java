5ea2c8ac27be972e2e1846a9ef8ad43e61c51794
AlexWedon <alex_wedon@hotmail.com>
2023-01-21 17:03:21 +0300

    Group 1 practice has added

--- src/MerhabaGroupOneSakinleri/HerkeseSelam.java
+++ src/MerhabaGroupOneSakinleri/HerkeseSelam.java
@@ -3,10 +3,6 @@ package MerhabaGroupOneSakinleri;
 public class HerkeseSelam {
     public static void main(String[] args) {
         System.out.println("Grup 1 Üyeleri Nasılsınız? İşler Nasıl Gidiyor?");
-        //Sağol Rıdvan.
-        int a=5;
-        int b=6;
-        int toplam=a+b;
-        System.out.println(toplam);
+
     }
 }
